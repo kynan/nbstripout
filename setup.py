d4e70f9f146b65b55c1e04955913906dd22eed4a
ooiM <windingooie@yahoo.co.jp>
2015-09-13 00:31:20 +0100

    More metadata in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup
 
+with open('README.rst') as f:
+    long_description = f.read()
+
 setup(name='nbstripout',
       version='0.1.0',
+      author='Min RK',
+      author_email='benjaminrk@gmail.com',
+      maintainer='Florian Rathgeber',
+      maintainer_email='florian.rathgeber@gmail.com',
+      url='https://github.com/kynan/nbstripout',
+      description='Strips outputs from Jupyter and IPython notebooks',
+      long_description=long_description,
       py_modules=['nbstripout'],
       entry_points={
           'console_scripts': [
