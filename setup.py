4877dc2335036206e5132533692722c932bfd138
ooiM <windingooie@yahoo.co.jp>
2016-01-18 16:51:03 -0700

    API: Start branch 0.2.0. Only process .ipynb files without -f
    - Processes multiple files.
    - Ignores all files but .ipynb files by default.
    - Added -f flag to force stripping other files.
    - This is the start of the 0.2.x branch which is maintained for now
      in a separate fork.

--- setup.py
+++ setup.py
@@ -4,12 +4,20 @@ with open('README.rst') as f:
     long_description = f.read()
 
 setup(name='nbstripout',
-      version='0.1.0',
+      version='0.2.0',
       author='Min RK',
       author_email='benjaminrk@gmail.com',
-      maintainer='Florian Rathgeber',
-      maintainer_email='florian.rathgeber@gmail.com',
-      url='https://github.com/kynan/nbstripout',
+
+      # 0.1 branch
+      # maintainer='Florian Rathgeber',
+      # maintainer_email='florian.rathgeber@gmail.com',
+      # url='https://github.com/kynan/nbstripout',
+
+      # 0.2 branch
+      maintainer='Michael McNeil Forbes',
+      maintainer_email='michael.forbes+python@gmail.com',
+      url='https://github.com/mforbes/nbstripout',
+
       description='Strips outputs from Jupyter and IPython notebooks',
       long_description=long_description,
       py_modules=['nbstripout'],
