90554dbb930465e9a3f00f00077570f3a7b68ab0
Florian Rathgeber <florian.rathgeber@gmail.com>
2016-03-05 19:34:56 +0000

    setup: no longer require pytest-flake8 == 0.1
    pytest-cram already requires pytest >= 2.8 anyway.

--- setup.py
+++ setup.py
@@ -8,9 +8,8 @@ setup_requires = [
 ]
 
 tests_require = [
-    'pytest',  # We cannot pin the pytest version due to inconsistent Travis
-               # environment https://github.com/travis-ci/travis-ci/issues/5635
-    'pytest-flake8 == 0.1',
+    'pytest',
+    'pytest-flake8',
     'pytest-cram',
 ]
 
