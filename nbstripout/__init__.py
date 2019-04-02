bb3cc672bc4cc9dd0c104564cecb3b13a8c8e29a
ooiM <windingooie@yahoo.co.jp>
2019-04-02 23:45:41 +0100

    Add package docstring

--- nbstripout/__init__.py
+++ nbstripout/__init__.py
@@ -1,4 +1,5 @@
-from ._nbstripout import install, uninstall, status, main
+from ._nbstripout import install, uninstall, status, main, __doc__ as docstring
 from ._utils import pop_recursive, strip_output
 __all__ = ["install", "uninstall", "status", "main",
            "pop_recursive", "strip_output"]
+__doc__ = docstring
