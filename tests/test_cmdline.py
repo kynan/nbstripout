d36ec887a2a076bd81347f5e586a0c6489970cf5
Andrew O'Brien <andrew.obrien@veitchlister.com>
2022-11-13 00:58:44 +1000

    dropping cram

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -3,7 +3,7 @@ from subprocess import Popen, PIPE
 from typing import List
 
 
-def run_nbstripout_compare_output(input_file: Path, expected_file: Path, args: List[str] = []):
+def run_nbstripout_compare_output(input_file: Path, expected_file: Path, args: List[str]):
     with open(input_file, mode="r") as f:
         input = f.read().encode()
 
@@ -17,12 +17,20 @@ def run_nbstripout_compare_output(input_file: Path, expected_file: Path, args: L
 
 
 def test_strip_init_cells():
-    run_nbstripout_compare_output(Path("tests/test_strip_init_cells.ipynb"), Path("tests/test-strip-init-cells.t"), ["--strip-init-cells"])
+    run_nbstripout_compare_output(Path("tests/test_strip_init_cells.ipynb"), Path("tests/test_strip_init_cells.ipynb.expected"), ["--strip-init-cells"])
 
 
-def test_drop_empty_cells_dont_drop():
-    run_nbstripout_compare_output(Path("tests/test_drop_empty_cells.ipynb"), Path("tests/test_drop_empty_cells_dontdrop.ipynb.expected"))
+def test_drop_empty_cells_dontdrop():
+    run_nbstripout_compare_output(Path("tests/test_drop_empty_cells.ipynb"), Path("tests/test_drop_empty_cells_dontdrop.ipynb.expected"), [])
 
 
 def test_drop_empty_cells():
     run_nbstripout_compare_output(Path("tests/test_drop_empty_cells.ipynb"), Path("tests/test_drop_empty_cells.ipynb.expected"), ["--drop-empty-cells"])
+
+
+def test_drop_tagged_cells_dontdrop():
+    run_nbstripout_compare_output(Path("tests/test_drop_tagged_cells.ipynb"), Path("tests/test_drop_tagged_cells_dontdrop.ipynb.expected"), [])
+
+
+def test_drop_tagged_cells():
+    run_nbstripout_compare_output(Path("tests/test_drop_tagged_cells.ipynb"), Path("tests/test_drop_tagged_cells.ipynb.expected"), ['--drop-tagged-cells=test'])
