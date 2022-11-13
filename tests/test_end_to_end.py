ac53364956ea058e6dbc4b61e9eeb4f8c182da0f
Andrew O'Brien <andrew.obrien@veitchlister.com>
2022-11-13 10:48:11 +1000

    implement dry-run tests

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -1,9 +1,12 @@
 import os
 from pathlib import Path
+import re
 from subprocess import Popen, PIPE
 from typing import List
 
 import pytest
+
+NOTEBOOKS_FOLDER = Path("tests/e2e_notebooks")
 
 TEST_CASES = [
     ("test_strip_init_cells.ipynb", "test_strip_init_cells.ipynb.expected", ["--strip-init-cells"]),
@@ -13,7 +16,10 @@ TEST_CASES = [
     ("test_drop_tagged_cells.ipynb", "test_drop_tagged_cells.ipynb.expected", ['--drop-tagged-cells=test']),
 ]
 
-NOTEBOOKS_FOLDER = Path("tests/e2e_notebooks")
+DRY_RUN_CASES = [
+    ("test_metadata.ipynb", []),
+    ("test_zeppelin.zpln", ["--mode", "zeppelin"]),
+]
 
 
 def get_nbstripout_exe():
@@ -34,3 +40,26 @@ def test_end_to_end_nbstripout(input_file: str, expected_file: str, args: List[s
     output = pc.communicate(input=input)[0]
 
     assert output == expected
+
+
+@pytest.mark.parametrize("input_file, extra_args", DRY_RUN_CASES)
+def test_dry_run_stdin(input_file: str, extra_args: List[str]):
+    with open(NOTEBOOKS_FOLDER / input_file, mode="r") as f:
+        input = f.read().encode()
+
+    expected = b"Dry run: would have stripped input from stdin\n"
+
+    pc = Popen([get_nbstripout_exe(), "--dry-run"] + extra_args, stdin=PIPE, stdout=PIPE)
+    output = pc.communicate(input=input)[0]
+
+    assert output == expected
+
+
+@pytest.mark.parametrize("input_file, extra_args", DRY_RUN_CASES)
+def test_dry_run_args(input_file: str, extra_args: List[str]):
+    expected_regex = re.compile(f"Dry run: would have stripped .*[/\\\\]{input_file}\n".encode())
+
+    pc = Popen([get_nbstripout_exe(), NOTEBOOKS_FOLDER / input_file, "--dry-run", ] + extra_args, stdout=PIPE)
+    output = pc.communicate()[0]
+
+    assert expected_regex.match(output)
