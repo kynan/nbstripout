0026124dd8f1638ec8b272dea6594abd0c199ad7
Andrew O'Brien <andrew.obrien@veitchlister.com>
2022-11-13 11:58:25 +1000

    finish end to end tests. Fix for python<3.7

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -1,8 +1,8 @@
 import os
 from pathlib import Path
 import re
-from subprocess import run
-from typing import List
+from subprocess import run, PIPE
+from typing import List, Union
 
 import pytest
 
@@ -14,7 +14,16 @@ TEST_CASES = [
     ("test_drop_tagged_cells.ipynb", "test_drop_tagged_cells_dontdrop.ipynb.expected", []),
     ("test_drop_tagged_cells.ipynb", "test_drop_tagged_cells.ipynb.expected", ['--drop-tagged-cells=test']),
     ("test_execution_timing.ipynb", "test_execution_timing.ipynb.expected", []),
+    ("test_max_size.ipynb", "test_max_size.ipynb.expected", ["--max-size", "50"]),
+    ("test_metadata.ipynb", "test_metadata.ipynb.expected", []),
+    ("test_metadata.ipynb", "test_metadata_extra_keys.ipynb.expected", ["--extra-keys", "metadata.kernelspec metadata.language_info"]),
+    ("test_metadata.ipynb", "test_metadata_keep_count.ipynb.expected", ["--keep-count"]),
+    ("test_metadata.ipynb", "test_metadata_keep_output.ipynb.expected", ["--keep-output"]),
+    ("test_metadata.ipynb", "test_metadata_keep_output_keep_count.ipynb.expected", ["--keep-output", "--keep-count"]),
+    ("test_metadata_notebook.ipynb", "test_metadata_notebook.ipynb", []),
+    ("test_metadata_period.ipynb", "test_metadata_period.ipynb.expected", ["--extra-keys", "cell.metadata.application/vnd.databricks.v1+cell metadata.application/vnd.databricks.v1+notebook"]),
     ("test_strip_init_cells.ipynb", "test_strip_init_cells.ipynb.expected", ["--strip-init-cells"]),
+    ("test_nbformat2.ipynb", "test_nbformat2.ipynb.expected", []),
     ("test_unicode.ipynb", "test_unicode.ipynb.expected", []),
     ("test_widgets.ipynb", "test_widgets.ipynb.expected", []),
     ("test_zeppelin.zpln", "test_zeppelin.zpln.expected", ["--mode", "zeppelin"]),
@@ -23,6 +32,11 @@ TEST_CASES = [
 DRY_RUN_CASES = [
     ("test_metadata.ipynb", []),
     ("test_zeppelin.zpln", ["--mode", "zeppelin"]),
+]
+
+ERR_OUTPUT_CASES = [
+    ("test_metadata.ipynb", ["Ignoring invalid extra key `invalid`", "Ignoring invalid extra key `foo.invalid`"], ["--extra-keys", "invalid foo.invalid"]),
+    ("test_metadata_exception.ipynb", [re.compile(".*MetadataError: cell metadata contradicts tags: `keep_output` is false, but `keep_output` in tags")], [])
 ]
 
 
@@ -38,7 +52,7 @@ def test_end_to_end_nbstripout(input_file: str, expected_file: str, args: List[s
         expected = f.read()
 
     with open(NOTEBOOKS_FOLDER / input_file, mode="r") as f:
-        pc = run([get_nbstripout_exe()] + args, stdin=f, capture_output=True, text=True)
+        pc = run([get_nbstripout_exe()] + args, stdin=f, stdout=PIPE, text=True)
         output = pc.stdout
 
     assert output == expected
@@ -49,7 +63,7 @@ def test_dry_run_stdin(input_file: str, extra_args: List[str]):
     expected = "Dry run: would have stripped input from stdin\n"
 
     with open(NOTEBOOKS_FOLDER / input_file, mode="r") as f:
-        pc = run([get_nbstripout_exe(), "--dry-run"] + extra_args, stdin=f, capture_output=True, text=True)
+        pc = run([get_nbstripout_exe(), "--dry-run"] + extra_args, stdin=f, stdout=PIPE, text=True)
         output = pc.stdout
 
     assert output == expected
@@ -59,7 +73,20 @@ def test_dry_run_stdin(input_file: str, extra_args: List[str]):
 def test_dry_run_args(input_file: str, extra_args: List[str]):
     expected_regex = re.compile(f"Dry run: would have stripped .*[/\\\\]{input_file}\n")
 
-    pc = run([get_nbstripout_exe(), str(NOTEBOOKS_FOLDER / input_file), "--dry-run", ] + extra_args, capture_output=True, text=True)
+    pc = run([get_nbstripout_exe(), str(NOTEBOOKS_FOLDER / input_file), "--dry-run", ] + extra_args, stdout=PIPE, text=True)
     output = pc.stdout
 
     assert expected_regex.match(output)
+
+
+@pytest.mark.parametrize("input_file, expected_errs, extra_args", ERR_OUTPUT_CASES)
+def test_make_errors(input_file: str, expected_errs: List[Union[str, re.Pattern]], extra_args: List[str]):
+    with open(NOTEBOOKS_FOLDER / input_file, mode="r") as f:
+        pc = run([get_nbstripout_exe(), "--dry-run"] + extra_args, stdin=f, stderr=PIPE, text=True)
+        err_output = pc.stderr
+
+    for e in expected_errs:
+        if isinstance(e, re.Pattern):
+            assert e.search(err_output)
+        else:
+            assert e in err_output
