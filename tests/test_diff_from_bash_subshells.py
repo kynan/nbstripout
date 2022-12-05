6c53f600e7f36e7a7373c7b3005e214aa2568bd2
Andrew O'Brien <andrew.obrien@veitchlister.com>
2022-12-05 23:49:27 +1000

    bash diff

--- tests/test_diff_from_bash_subshells.py
+++ tests/test_diff_from_bash_subshells.py
@@ -7,20 +7,31 @@ import pytest
 NOTEBOOKS_FOLDER = Path("tests").absolute()
 
 
-def test_diff_with_process_substitution(pytester: pytest.Pytester):
+def test_diff_with_process_substitution_nodiff(pytester: pytest.Pytester):
     if sys.platform.startswith("win"):
         pytest.skip("test requires proper bash shell", allow_module_level=True)
 
     r = pytester.run(
-        'bash', 
-        '-c', 
-        f'"diff <( $nbstripout -t ${NOTEBOOKS_FOLDER / "test_diff.ipynb"} ) <( $nbstripout -t ${NOTEBOOKS_FOLDER / "test_diff_output.ipynb"} )"'
+        'bash',
+        '-c',
+        f'"diff <( $nbstripout -t ${NOTEBOOKS_FOLDER / "test_diff.ipynb"} ) <( $nbstripout -t ${NOTEBOOKS_FOLDER / "test_diff_output.ipynb"} )"',
     )
-    assert len(r.stdout.lines) == 0
+    assert not r.outlines
+    assert r.ret == 0
+
+
+def test_diff_with_process_substitution_diff(pytester: pytest.Pytester):
+    if sys.platform.startswith("win"):
+        pytest.skip("test requires proper bash shell", allow_module_level=True)
 
     r = pytester.run(
-        'bash', 
-        '-c', 
-        f'"diff <( $nbstripout -t ${NOTEBOOKS_FOLDER / "test_diff.ipynb"} ) <( $nbstripout -t ${NOTEBOOKS_FOLDER / "test_diff_different.ipynb"} )"'
+        'bash',
+        '-c',
+        f'"diff <( $nbstripout -t ${NOTEBOOKS_FOLDER / "test_diff.ipynb"} ) <( $nbstripout -t ${NOTEBOOKS_FOLDER / "test_diff_different.ipynb"} )"',
     )
-    assert len(r.stdout.lines) == 0
+    assert r.stdout.re_match_lines(r"""(.*)
+  <     "print(\"aou\")"
+  ---
+  (.*\"print\(\\\"aou now it is different\\\"\)\")
+  """.splitlines())
+    assert r.ret == 1
