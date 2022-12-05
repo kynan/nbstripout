489774a59afb4a077d576ce997a0749676a6cfed
Andrew O'Brien <andrew.obrien@veitchlister.com>
2022-12-06 00:13:36 +1000

    bash diff

--- tests/test_diff_from_bash_subshells.py
+++ tests/test_diff_from_bash_subshells.py
@@ -12,9 +12,9 @@ def test_diff_with_process_substitution_nodiff(pytester: pytest.Pytester):
         pytest.skip("test requires proper bash shell", allow_module_level=True)
 
     r = pytester.run(
-        'bash',
-        '-c',
-        f'diff <( nbstripout -t {NOTEBOOKS_FOLDER / "test_diff.ipynb"} ) <( nbstripout -t {NOTEBOOKS_FOLDER / "test_diff_output.ipynb"} )',
+        'diff',
+        '<(', 'nbstripout', '-t', NOTEBOOKS_FOLDER / "test_diff.ipynb", ')',
+        '<(', 'nbstripout', '-t', NOTEBOOKS_FOLDER / "test_diff_output.ipynb", ')',
     )
     assert not r.outlines
     assert r.ret == 0
@@ -25,9 +25,9 @@ def test_diff_with_process_substitution_diff(pytester: pytest.Pytester):
         pytest.skip("test requires proper bash shell", allow_module_level=True)
 
     r = pytester.run(
-        'bash',
-        '-c',
-        f'diff <( nbstripout -t {NOTEBOOKS_FOLDER / "test_diff.ipynb"} ) <( nbstripout -t {NOTEBOOKS_FOLDER / "test_diff_different.ipynb"} )',
+        'diff',
+        '<(', 'nbstripout', '-t', NOTEBOOKS_FOLDER / "test_diff.ipynb", ')',
+        '<(', 'nbstripout', '-t', NOTEBOOKS_FOLDER / "test_diff_different.ipynb", ')',
     )
     r.stdout.re_match_lines(r"""(.*)
 <     "print(\"aou\")"
