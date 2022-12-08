cb473225c59075074e1efdb04810ac0e8482ab0d
Andrew O'Brien <ar.obrien@gmail.com>
2022-12-09 06:16:13 +1000

    Ditch Cram (#178)
    * test git integration using pytester
    * convert git status and install tests to pytester
    * shift test-diff to pytester and drop cram
    
    Co-authored-by: Andrew O'Brien <andrew.obrien@veitchlister.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,35 +3,35 @@ import pytest
 from nbstripout._utils import pop_recursive
 
 
-def testdict():
+def make_dict():
     return {'a': {'b': 1, 'c': 2, 'd.e': 3, 'f': {'g': 4}}}
 
 
-def testdata(default=None):
+def make_data(default=None):
     return [
         ('a.c', 2, {'a': {'b': 1, 'd.e': 3, 'f': {'g': 4}}}),
         ('a.d.e', 3, {'a': {'b': 1, 'c': 2, 'f': {'g': 4}}}),
         ('a.f', {'g': 4}, {'a': {'b': 1, 'c': 2, 'd.e': 3}}),
         ('a.f.g', 4, {'a': {'b': 1, 'c': 2, 'd.e': 3, 'f': {}}}),
         ('a', {'b': 1, 'c': 2, 'd.e': 3, 'f': {'g': 4}}, {}),
-        ('notfound', default, testdict()),
-        ('a.notfound', default, testdict()),
-        ('a.b.notfound', default, testdict()),
+        ('notfound', default, make_dict()),
+        ('a.notfound', default, make_dict()),
+        ('a.b.notfound', default, make_dict()),
     ]
 
 
 @pytest.fixture
 def d():
-    return testdict()
+    return make_dict()
 
 
-@pytest.mark.parametrize(('key', 'res', 'remainder'), testdata())
+@pytest.mark.parametrize(('key', 'res', 'remainder'), make_data())
 def test_pop_recursive(d, key, res, remainder):
     assert pop_recursive(d, key) == res
     assert d == remainder
 
 
-@pytest.mark.parametrize(('key', 'res', 'remainder'), testdata(default=0))
+@pytest.mark.parametrize(('key', 'res', 'remainder'), make_data(default=0))
 def test_pop_recursive_default(d, key, res, remainder):
     assert pop_recursive(d, key, default=0) == res
     assert d == remainder
