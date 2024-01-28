c4b55d72b0813d9e080d56348c1bd6aa0cfae1c8
Florian Rathgeber <florian.rathgeber@gmail.com>
2024-01-28 22:49:57 +0100

    Drop support for Python 3.7, require Python 3.8

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(name='nbstripout',
       },
 
       install_requires=install_requires,
-      python_requires='>=3.6',
+      python_requires='>=3.8',
 
       classifiers=[
           "Development Status :: 4 - Beta",
@@ -37,7 +37,6 @@ setup(name='nbstripout',
           "Intended Audience :: Developers",
           "Programming Language :: Python",
           "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.7",
           "Programming Language :: Python :: 3.8",
           "Programming Language :: Python :: 3.9",
           "Programming Language :: Python :: 3.10",
